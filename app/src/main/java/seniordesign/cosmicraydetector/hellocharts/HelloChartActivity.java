98dc82beba74500cc4842a818ab9ad3c3cbca400
Tochukwu Akujuo <tochukwu.akujuo@stonybrook.edu>
2015-05-01 00:41:20 -0400

    Added Logic for Graphing Any X to Any Y

--- app/src/main/java/seniordesign/cosmicraydetector/hellocharts/HelloChartActivity.java
+++ app/src/main/java/seniordesign/cosmicraydetector/hellocharts/HelloChartActivity.java
@@ -3,6 +3,7 @@ package seniordesign.cosmicraydetector.hellocharts;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -41,13 +42,67 @@ public class HelloChartActivity extends ActionBarActivity {
         String yType = GraphActivity.yType;
 
 
-
-
-
-
+        //List of vales to be graphed
         List<PointValue> values = new ArrayList<PointValue>();
+
+        //Graph Generation
+
         float xValue, yValue;
+
+        /*
+        * TODO: Add retrieval of specified range of SensorData
+        */
         SensorData sensorData;
+        Set<Long> keySet = MainActivity.sensorDataMap.keySet();
+
+        for(Long key : keySet){
+
+            sensorData = MainActivity.sensorDataMap.get(key);
+
+            //get X based on type
+            if(xType.equalsIgnoreCase("temperature")){
+                xValue = sensorData.getTemperature().floatValue();
+            }
+            else if(xType.equalsIgnoreCase("pressure")){
+                xValue = sensorData.getPressure().floatValue();
+            }
+            else if(xType.equalsIgnoreCase("date")){
+                xValue = new Long(sensorData.getDate().getTime()).floatValue();
+            }
+            else if(xType.equalsIgnoreCase("humidity")){
+                xValue = sensorData.getHumidity().floatValue();
+            }
+            else{// count
+                xValue = sensorData.getCount().floatValue();
+            }
+
+
+            //get Y based on type
+            if(xType.equalsIgnoreCase("temperature")){
+                yValue = sensorData.getTemperature().floatValue();
+            }
+            else if(xType.equalsIgnoreCase("pressure")){
+                yValue = sensorData.getPressure().floatValue();
+            }
+            else if(xType.equalsIgnoreCase("date")){
+                yValue = new Long(sensorData.getDate().getTime()).floatValue();
+            }
+            else if(xType.equalsIgnoreCase("humidity")){
+                yValue = sensorData.getHumidity().floatValue();
+            }
+            else{//count
+                yValue = sensorData.getCount().floatValue();
+            }
+
+            //TODO: Add Label Generating for dates, and may append units
+
+            //Create point and add to list
+            PointValue point = new PointValue(xValue,yValue);
+            values.add(point);
+
+
+        }// end of for loop
+
 
         /*
         Set<Long> keySet = MainActivity.sensorDataMap.keySet();
@@ -58,26 +113,27 @@ public class HelloChartActivity extends ActionBarActivity {
         }
 
         */
-
-
-        Set<Long> keySet = MainActivity.sensorDataMap.keySet();
-        for (Long key : keySet){
+       /* for (Long key : keySet){
             sensorData = MainActivity.sensorDataMap.get(key);
-            values.add(new PointValue(new Long(sensorData.getDate().getTime()).floatValue(),
-                    sensorData.getCount().floatValue()));
-        }
+            PointValue point = new PointValue(new Long(sensorData.getDate().getTime()).floatValue(),
+                    sensorData.getCount().floatValue());
+
+            point.setTarget(point.getX(),point.getY());
+            point.set(point.getX(),0f);
+
+            values.add(point);
+
+        }*/
 
 
-
-        Line line = new Line(values).setColor(Color.BLUE).setCubic(true);
+        Line line = new Line(values).setColor(Color.BLUE).setCubic(false);
         List<Line> lines = new ArrayList<Line>(1);
 
+        //Initial Line Setup
         line.setShape(ValueShape.CIRCLE);
-        line.setCubic(false);
         line.setHasLabelsOnlyForSelected(true);
         line.setHasPoints(true);
         line.setHasLines(true);
-
 
         lines.add(line);
 
@@ -87,28 +143,60 @@ public class HelloChartActivity extends ActionBarActivity {
         Axis xAxis = new Axis().setHasLines(true);
         Axis yAxis = new Axis().setHasLines(true);
 
-        xAxis.setName("X Axis");
+
+        //X AXIS SETUP
+        String xAxisLabel, yAxisLabel;
+        if(xType.equalsIgnoreCase("temperature")){
+            xAxisLabel = "Temperature (C)";
+        }
+        else if(xType.equalsIgnoreCase("pressure")){
+            xAxisLabel = "Pressure (C)";
+        }
+        else if(xType.equalsIgnoreCase("date")){
+            xAxisLabel = "Date (MM-DD-YY)";
+        }
+        else if(xType.equalsIgnoreCase("humidity")){
+            xAxisLabel = "Humidity (%)";
+        }
+        else{// count
+            xAxisLabel = "Cosmic Ray Counts";
+        }
+        xAxis.setName(xAxisLabel);
         xAxis.setTextColor(Color.BLACK);
         xAxis.setHasTiltedLabels(true);
 
-        yAxis.setName("Y Axis");
+
+        //Y AXIS SETUP
+        if(yType.equalsIgnoreCase("temperature")){
+            yAxisLabel = "Temperature (C)";
+        }
+        else if(yType.equalsIgnoreCase("pressure")){
+            yAxisLabel = "Pressure (C)";
+        }
+        else if(yType.equalsIgnoreCase("date")){
+            yAxisLabel = "Date (MM-DD-YY)";
+        }
+        else if(yType.equalsIgnoreCase("humidity")){
+            yAxisLabel = "Humidity (%)";
+        }
+        else{// count
+            yAxisLabel = "Cosmic Ray Counts";
+        }
+        yAxis.setName(yAxisLabel);
         yAxis.setTextColor(Color.BLACK);
 
-
+        //Setup Chart Data
         data.setAxisXBottom(xAxis);
         data.setAxisYLeft(yAxis);
-
         data.setBaseValue(Float.NEGATIVE_INFINITY);
 
+        //Get ChartView
         LineChartView chart = (LineChartView) findViewById(R.id.chart);
-
         chart.setLineChartData(data);
+        //Default zoom will be scrolling left to right only
         chart.setZoomType(ZoomType.HORIZONTAL);
 
-
-
-
-
+        //chart.startDataAnimation();
 
     }
 
